bf172076991445959256c43fb037b4ea43fccfa4
GGH <1421938824@qq.com>
2022-01-25 20:06:31 +0800

    完善方法

--- src/main/java/com/gzcss/mybatis/sqlsession/defaults/DefaultSqlSession.java
+++ src/main/java/com/gzcss/mybatis/sqlsession/defaults/DefaultSqlSession.java
@@ -1,13 +1,30 @@
 package com.gzcss.mybatis.sqlsession.defaults;
 
+import com.gzcss.cfg.Configuration;
 import com.gzcss.mybatis.sqlsession.SqlSession;
+import com.gzcss.mybatis.sqlsession.proxy.MapperProxy;
+
+import java.lang.reflect.Proxy;
 
 public class DefaultSqlSession implements SqlSession {
-    @Override
-    public <T> T getMapper(Class<T> daoClass) {
-        return null;
+
+    private Configuration cfg;
+
+    public DefaultSqlSession(Configuration cfg){
+        this.cfg = cfg;
     }
 
+    /*
+    * 创建代理对象
+    * */
+    @Override
+    public <T> T getMapper(Class<T> daoInterfaceClass) {
+        Proxy.newProxyInstance(daoInterfaceClass.getClassLoader(),new Class[]{daoInterfaceClass},new MapperProxy(cfg.getMappers()));
+         return null;
+    }
+    /*
+    *   释放资源
+    * */
     @Override
     public void close() {
 
